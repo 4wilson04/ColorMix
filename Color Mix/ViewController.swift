3843d44fdede736eb6ac2e23155cfe9af9719433
4wilson04 <60400038+4wilson04@users.noreply.github.com>
2020-02-03 12:00:58 -0500

    Color CHange
    #CyanAndPurpleGang

--- Color Mix/ViewController.swift
+++ Color Mix/ViewController.swift
@@ -2,8 +2,8 @@
 //  ViewController.swift
 //  Color Mix
 //
-//  Created by WOLF, CHRISTOPHER (LYHS) on 1/31/20.
-//  Copyright © 2020 Ava Wilson. All rights reserved.
+//  Created by WoLf, ChRiStOpHeR (LYHS) on 1/31/20.
+//  Copyright © 2020 Ava Wilson . All rights reserved.
 //
 
 import UIKit
@@ -11,6 +11,12 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var colorView: UIView!
+    
+    let onColor = UIColor(red: 0.05, green: 0.50, blue: 0.55, alpha: 1)
+    let offColor = UIColor(red: 0.50, green: 0.05, blue: 0.55, alpha: 1)
+    let textPurple = "purple"
+    let textCyan = "Cyan"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let color = UIColor(red: 0.05, green: 0.50, blue: 0.55, alpha: 1)
@@ -18,6 +24,15 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func switchChanged(_ sender: UISwitch) {
+        if sender.isOn {
+            colorView.backgroundColor = onColor
+        } else {
+            colorView.backgroundColor = offColor
+        }
+    }
+    
+   
+    }
 
-}
 

41a359d488f45057f0a8ca3ad64a9a20965136a4
4wilson04 <60400038+4wilson04@users.noreply.github.com>
2020-02-07 11:26:42 -0500

    Three Color Mixing
    Two State RGB

--- Color Mix/ViewController.swift
+++ Color Mix/ViewController.swift
@@ -12,27 +12,25 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var colorView: UIView!
     
-    let onColor = UIColor(red: 0.05, green: 0.50, blue: 0.55, alpha: 1)
-    let offColor = UIColor(red: 0.50, green: 0.05, blue: 0.55, alpha: 1)
-    let textPurple = "purple"
-    let textCyan = "Cyan"
+    @IBOutlet weak var redSwitch: UISwitch!
+    @IBOutlet weak var blueSwitch: UISwitch!
+    @IBOutlet weak var greenSwitch: UISwitch!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let color = UIColor(red: 0.05, green: 0.50, blue: 0.55, alpha: 1)
-        colorView.backgroundColor = color
-        // Do any additional setup after loading the view.
+        setColor()
     }
 
-    @IBAction func switchChanged(_ sender: UISwitch) {
-        if sender.isOn {
-            colorView.backgroundColor = onColor
-        } else {
-            colorView.backgroundColor = offColor
-        }
+    @IBAction func switchChanged(_ sender: UISwitch){
+        setColor()
     }
     
-   
+    func setColor() {
+        let red: CGFloat = redSwitch.isOn ? 1:0
+        let green: CGFloat = greenSwitch.isOn ? 1:0
+        let blue: CGFloat = blueSwitch.isOn ? 1:0
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 0.49)
+        colorView.backgroundColor = color
     }
-
+}
 
